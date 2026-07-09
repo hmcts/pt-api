593b620b88253fdf2b518191a9484450ea54570e
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-07-09 10:07:08 +0100

    feat: add new fields to PTCaseEntity and update database schema

--- src/main/java/uk/gov/hmcts/reform/pt/entity/PTCaseEntity.java
+++ src/main/java/uk/gov/hmcts/reform/pt/entity/PTCaseEntity.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.pt.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
@@ -11,22 +14,33 @@ import lombok.Getter;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import uk.gov.hmcts.reform.pt.ccd.domain.ApplicationType;
 
 import java.util.UUID;
 
 @Entity
-@Table(name = "pt_case")
 @Setter
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "pt_case")
 public class PTCaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     @Version
     private Integer version;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "application_type")
+    private ApplicationType applicationType;
+
     private Long caseReference;
-    private String applicantForename;
+    private String applicantFirstName;
+    private String applicantLastName;
+    private String email;
+    private String postcode;
+    private UUID idamUserId;
 }
---
+++ src/main/resources/db/migration/V2__update_pt_case.sql
@@ -0,0 +1,8 @@
+ALTER TABLE pt_case
+  ADD COLUMN idam_user_id UUID,
+  ADD COLUMN application_type VARCHAR(100),
+  ADD COLUMN applicant_last_name VARCHAR(100),
+  ADD COLUMN email VARCHAR(255),
+  ADD COLUMN postcode VARCHAR(20);
+
+ALTER TABLE pt_case RENAME COLUMN applicant_forename TO applicant_first_name;
