358337a8f829d5270f428db32f4d123a75075b24
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2026-07-15 16:44:44 +0100

    Fix checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/pt/functional/testutils/PtIdamTokenClient.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pt/functional/testutils/PtIdamTokenClient.java
@@ -7,7 +7,10 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
-import static uk.gov.hmcts.reform.pt.functional.config.AuthConfig.*;
+import static uk.gov.hmcts.reform.pt.functional.config.AuthConfig.CLIENT_ID;
+import static uk.gov.hmcts.reform.pt.functional.config.AuthConfig.SCOPE;
+import static uk.gov.hmcts.reform.pt.functional.config.AuthConfig.GRANT_TYPE;
+import static uk.gov.hmcts.reform.pt.functional.config.AuthConfig.ENDPOINT;
 import static uk.gov.hmcts.reform.pt.functional.testutils.EnvUtils.getEnv;
 
 public class PtIdamTokenClient {
