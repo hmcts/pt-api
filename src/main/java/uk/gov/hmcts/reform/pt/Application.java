5b8014891daf9016013847b40281bead432cc870
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-07-27 12:01:04 +0100

    feat: add doc gen config

--- src/main/java/uk/gov/hmcts/reform/pt/Application.java
+++ src/main/java/uk/gov/hmcts/reform/pt/Application.java
@@ -8,23 +8,29 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 import uk.gov.hmcts.reform.pt.idam.IdamUserInfoApi;
 
 @EnableScheduling
-@ImportAutoConfiguration({FeignAutoConfiguration.class, FeignClientsConfiguration.class,
-    HttpMessageConvertersAutoConfiguration.class})
+@ImportAutoConfiguration({
+    FeignAutoConfiguration.class,
+    FeignClientsConfiguration.class,
+    HttpMessageConvertersAutoConfiguration.class
+})
 @EnableFeignClients(
     clients = {
         IdamUserInfoApi.class,
-        IdamApi.class, // not used by pt-api code; required so ccd-sdk's IdamClient can wire.
+        IdamApi.class, // not used by pt-api code; required so ccd-sdk's IdamClient can wire
+        CaseDocumentClientApi.class
     }
 )
 @SpringBootApplication(
     scanBasePackages = {
         "uk.gov.hmcts.reform.pt",
         "uk.gov.hmcts.ccd.sdk",
-        "uk.gov.hmcts.reform.ccd.client"
+        "uk.gov.hmcts.reform.ccd.client",
+        "uk.gov.hmcts.reform.docassembly",
     })
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
