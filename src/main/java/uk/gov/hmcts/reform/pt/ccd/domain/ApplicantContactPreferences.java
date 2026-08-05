487c5ac1624755643eb654341d703f0089c1f76b
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-08-05 11:13:12 +0100

    feat: update contact preferences on update event (if contact preferences exist)

---
+++ src/main/java/uk/gov/hmcts/reform/pt/ccd/domain/ApplicantContactPreferences.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.pt.ccd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.reform.pt.ccd.accesscontrol.CitizenAccess;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicantContactPreferences {
+    @CCD(
+        label = "Applicant wants to receive text updates",
+        access = {CitizenAccess.class}
+    )
+    private YesOrNo textUpdates;
+
+    @CCD(
+        label = "Mobile phone number applicant want to receive text updates for",
+        access = {CitizenAccess.class}
+    )
+    private String textUpdatesPhoneNumber;
+
+    @CCD(
+        label = "Applicant wants to receive text updates",
+        access = {CitizenAccess.class}
+    )
+    private String phoneNumberForCalls;
+}
--- src/main/java/uk/gov/hmcts/reform/pt/ccd/domain/PTCase.java
+++ src/main/java/uk/gov/hmcts/reform/pt/ccd/domain/PTCase.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.pt.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
@@ -43,4 +44,7 @@ public class PTCase {
         access = {CitizenAccess.class}
     )
     private TenancyType tenancyType;
+
+    @JsonUnwrapped
+    private ApplicantContactPreferences applicantContactPreferences;
 }
