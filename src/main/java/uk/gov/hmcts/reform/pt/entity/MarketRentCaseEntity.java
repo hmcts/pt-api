b11ff6db7b4dc516435661c917568652e2e3a75e
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-08-05 11:53:24 +0100

    feat: update market rent case and tenancy details tables

--- src/main/java/uk/gov/hmcts/reform/pt/entity/MarketRentCaseEntity.java
+++ src/main/java/uk/gov/hmcts/reform/pt/entity/MarketRentCaseEntity.java
@@ -19,8 +19,10 @@ import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.reform.pt.ccd.domain.Frequency;
 import uk.gov.hmcts.reform.pt.ccd.domain.PropertyType;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
@@ -60,7 +62,7 @@ public class MarketRentCaseEntity extends AuditableEntity {
     private String rentingFlatDetails;
 
     @Column(length = 500)
-    private String rentingRoomsDetails;
+    private String rentingRoomDetails;
 
     @Column(length = 500)
     private String otherMethodOfRentDetails;
@@ -68,16 +70,8 @@ public class MarketRentCaseEntity extends AuditableEntity {
     @Column(length = 5000)
     private String floorplanManualDetails;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
-    private YesOrNo tenancyIncludeFacilities;
-
     @Column(length = 5000)
     private String propertyIndoorFeatures;
-
-    @Column(length = 5000)
-    private String otherFacilitiesDetails;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -86,6 +80,64 @@ public class MarketRentCaseEntity extends AuditableEntity {
 
     @Column(length = 5000)
     private String sharePropertyWithLandlordDetails;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private Frequency rentPaymentFrequency;
+
+    private BigDecimal rentCostWeekly;
+    private BigDecimal rentCostFortnightly;
+    private BigDecimal rentCostMonthly;
+    private BigDecimal rentCostYearly;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private YesOrNo rentIncludesCouncilTax;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private Frequency councilTaxFrequency;
+
+    private BigDecimal councilTaxCostWeekly;
+    private BigDecimal councilTaxCostFortnightly;
+    private BigDecimal councilTaxCostMonthly;
+    private BigDecimal councilTaxCostYearly;
+
+    @Column(length = 500)
+    private String councilTaxFrequencyAndCostDetails;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private Frequency utilitiesPaidFrequency;
+
+    private BigDecimal utilitiesCostWeekly;
+    private BigDecimal utilitiesCostFortnightly;
+    private BigDecimal utilitiesCostMonthly;
+    private BigDecimal utilitiesCostYearly;
+
+    @Column(length = 500)
+    private String utilitiesFrequencyAndCostDetails;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private YesOrNo otherHouseholdManagementCharges;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private YesOrNo rentInclusiveOfUtilityCharges;
+
+    private BigDecimal applicantSuggestedMonthlyMarketRent;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private YesOrNo additionalRentalServiceChargesVary;
+
+    @Column(length = 5000)
+    private String additionalRentalVaryingServiceChargesDetails;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "case_application_id")
---
+++ src/main/resources/db/migration/V6__tenancy_details_market_rents_add_cols.sql
@@ -0,0 +1,71 @@
+CREATE TYPE YES_NO_NOT_SURE AS ENUM ('YES', 'NO', 'NOT_SURE');
+CREATE TYPE FREQUENCY AS ENUM ('WEEKLY', 'FORTNIGHTLY', 'MONTHLY', 'YEARLY');
+
+ALTER TABLE tenancy_details
+  ADD COLUMN additional_services_provided_in_tenancy YES_NO,
+  ADD COLUMN additional_services_provided_in_tenancy_details VARCHAR (5000),
+  ADD COLUMN landlord_repairs_details VARCHAR (5000),
+  ADD COLUMN tenant_repairs_details VARCHAR (5000),
+  ADD COLUMN any_tenants_made_property_repairs YES_NO_NOT_SURE,
+  ADD COLUMN tenants_property_repairs_details VARCHAR(5000),
+  ADD COLUMN tenancy_include_facilities YES_NO,
+  ADD COLUMN other_facilities_details VARCHAR(5000),
+  ADD COLUMN furniture_provided_in_tenancy YES_NO,
+  ADD COLUMN furniture_provided_in_tenancy_details VARCHAR (5000),
+  ADD COLUMN tenancy_end_date TIMESTAMP,
+  ADD COLUMN current_tenancy_replace_original_tenancy YES_NO_NOT_SURE,
+  ADD COLUMN original_tenancy_start_date TIMESTAMP,
+  ADD COLUMN current_tenancy_start_date TIMESTAMP,
+  ADD COLUMN tribunal_previously_determined_tenancy_rent YES_NO,
+  ADD COLUMN previous_tribunal_case_reference VARCHAR (30);
+
+ALTER TABLE tenancy_details
+  ALTER COLUMN tenancy_type SET NOT NULL,
+  ALTER COLUMN additional_services_provided_in_tenancy SET NOT NULL,
+  ALTER COLUMN any_tenants_made_property_repairs SET NOT NULL,
+  ALTER COLUMN tenancy_include_facilities SET NOT NULL,
+  ALTER COLUMN furniture_provided_in_tenancy SET NOT NULL,
+  ALTER COLUMN tenancy_end_date SET NOT NULL,
+  ALTER COLUMN current_tenancy_replace_original_tenancy SET NOT NULL,
+  ALTER COLUMN current_tenancy_start_date SET NOT NULL,
+  ALTER COLUMN tribunal_previously_determined_tenancy_rent SET NOT NULL;
+
+
+ALTER TABLE market_rent_case
+  ADD COLUMN rent_payment_frequency FREQUENCY,
+  ADD COLUMN rent_cost_weekly NUMERIC(18,2),
+  ADD COLUMN rent_cost_fortnightly NUMERIC(18,2),
+  ADD COLUMN rent_cost_monthly NUMERIC(18,2),
+  ADD COLUMN rent_cost_yearly NUMERIC(18,2),
+  ADD COLUMN rent_includes_council_tax YES_NO,
+  ADD COLUMN council_tax_frequency FREQUENCY,
+  ADD COLUMN council_tax_cost_weekly NUMERIC(18,2),
+  ADD COLUMN council_tax_cost_fortnightly NUMERIC(18,2),
+  ADD COLUMN council_tax_cost_monthly NUMERIC(18,2),
+  ADD COLUMN council_tax_cost_yearly NUMERIC(18,2),
+  ADD COLUMN council_tax_frequency_and_cost_details VARCHAR(500),
+  ADD COLUMN utilities_paid_frequency FREQUENCY,
+  ADD COLUMN utilities_cost_weekly NUMERIC(18,2),
+  ADD COLUMN utilities_cost_fortnightly NUMERIC(18,2),
+  ADD COLUMN utilities_cost_monthly NUMERIC(18,2),
+  ADD COLUMN utilities_cost_yearly NUMERIC(18,2),
+  ADD COLUMN utilities_frequency_and_cost_details VARCHAR(500),
+  ADD COLUMN other_household_management_charges YES_NO,
+  ADD COLUMN rent_inclusive_of_utility_charges YES_NO,
+  ADD COLUMN applicant_suggested_monthly_market_rent NUMERIC(18,2),
+  ADD COLUMN additional_rental_service_charges_vary YES_NO,
+  ADD COLUMN additional_rental_varying_service_charges_details VARCHAR(5000);
+
+ALTER TABLE market_rent_case
+  ALTER COLUMN rent_includes_council_tax SET NOT NULL,
+  ALTER COLUMN other_household_management_charges SET NOT NULL,
+  ALTER COLUMN rent_inclusive_of_utility_charges SET NOT NULL,
+  ALTER COLUMN applicant_suggested_monthly_market_rent SET NOT NULL,
+  ALTER COLUMN additional_rental_service_charges_vary SET NOT NULL;
+
+ALTER TABLE market_rent_case
+  RENAME COLUMN renting_rooms_details TO renting_room_details;
+
+ALTER TABLE market_rent_case
+  DROP COLUMN tenancy_include_facilities,
+  DROP COLUMN other_facilities_details;
