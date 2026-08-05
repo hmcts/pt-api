e117eb6086aa1054d69941f4dead5d22a081a698
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2026-08-05 09:25:42 +0100

    FTs for application endpoint

--- src/functionalTest/java/uk/gov/hmcts/reform/pt/functional/steps/ApiSteps.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pt/functional/steps/ApiSteps.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.reform.pt.functional.steps;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import uk.gov.hmcts.reform.pt.functional.config.Endpoints;
 import uk.gov.hmcts.reform.pt.functional.config.TestConstants;
 import uk.gov.hmcts.reform.pt.functional.testutils.PtIdamTokenClient;
 import uk.gov.hmcts.reform.pt.functional.testutils.ServiceAuthenticationGenerator;
@@ -43,6 +46,13 @@ public class ApiSteps {
         SerenityRest.given().baseUri(baseUrl);
     }
 
+    @Step("a request is prepared with appropriate values")
+    public void requestIsPreparedWithAppropriateValues() {
+        request = SerenityRest.given()
+            .baseUri(baseUrl)
+            .contentType(ContentType.JSON);
+    }
+
     @Step("the request contains a valid service token for {0}")
     public void theRequestContainsValidServiceToken(String microservice) {
         final Map<String, String> serviceTokens = Map.of(
@@ -58,6 +68,17 @@ public class ApiSteps {
         request = request.header(TestConstants.SERVICE_AUTHORIZATION, validS2sToken);
     }
 
+    @Step("the request contains a valid IDAM token")
+    public void theRequestContainsValidIdamToken(PtIdamTokenClient.UserType user) {
+        String userToken = switch (user) {
+            case systemUser -> systemUserIdamToken;
+            case citizenUser -> citizenUserIdamToken;
+            case caseworkerUser -> caseworkerUserIdamToken;
+        };
+
+        request = request.header(TestConstants.AUTHORIZATION, "Bearer " + userToken);
+    }
+
     @Step("the request contains an unauthorised service token")
     public void theRequestContainsUnauthorisedServiceToken() {
         request = request.header(TestConstants.SERVICE_AUTHORIZATION, unauthorisedS2sToken);
@@ -67,6 +88,40 @@ public class ApiSteps {
     public void theRequestContainsExpiredServiceToken() {
         String expiredS2sToken = TestConstants.EXPIRED_S2S_TOKEN;
         request = request.header(TestConstants.SERVICE_AUTHORIZATION, expiredS2sToken);
+    }
+
+    @Step("a call is submitted to the {0} endpoint using a {1} request")
+    public void callIsSubmittedToTheEndpoint(String resource, String method) {
+        Endpoints resourceAPI = Endpoints.valueOf(resource);
+
+        response = switch (method.toUpperCase()) {
+            case "POST" -> request.when().post(resourceAPI.getResource());
+            case "GET" -> request.when().get(resourceAPI.getResource());
+            case "DELETE" -> request.when().delete(resourceAPI.getResource());
+            case "PUT" -> request.when().put(resourceAPI.getResource());
+            default -> throw new IllegalStateException("Unexpected value: " + method.toUpperCase());
+        };
+    }
+
+    @Step("Check status code is {0}")
+    public void checkStatusCode(int statusCode) {
+        if (response == null) {
+            throw new IllegalStateException("No response available. Did you call callIsSubmittedToTheEndpoint first?");
+        }
+        response.then().assertThat().statusCode(statusCode);
+    }
+
+    @Step("the response body is an empty array")
+    public void theResponseBodyIsAnEmptyArray() {
+        SerenityRest.then()
+            .assertThat()
+            .body("", Matchers.hasSize(0));
+    }
+
+    @Step("the request contains an expired IDAM token")
+    public void theRequestContainsExpiredIdamToken() {
+        String expiredIdamToken = TestConstants.EXPIRED_IDAM_TOKEN;
+        request = request.header(TestConstants.AUTHORIZATION, "Bearer " + expiredIdamToken);
     }
 
     @Step("Check Health")
---
+++ src/functionalTest/java/uk/gov/hmcts/reform/pt/functional/tests/ApplicationsEndpointTests.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.reform.pt.functional.tests;
+
+import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.annotations.Title;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.gov.hmcts.reform.pt.functional.config.TestConstants;
+import uk.gov.hmcts.reform.pt.functional.steps.BaseApi;
+import uk.gov.hmcts.reform.pt.functional.steps.ApiSteps;
+import uk.gov.hmcts.reform.pt.functional.testutils.PtIdamTokenClient;
+
+@Tag("Functional")
+@ExtendWith(SerenityJUnit5Extension.class)
+class ApplicationsEndpointTests extends BaseApi {
+
+    @Steps
+    ApiSteps apiSteps;
+
+    @Title("Applications Endpoint Tests - should return 200 when a user has no applications and return an empty list")
+    @Test
+    void applicationsEndpointTest200Scenario() {
+        apiSteps.requestIsPreparedWithAppropriateValues();
+        apiSteps.theRequestContainsValidServiceToken(TestConstants.PT_API);
+        apiSteps.theRequestContainsValidIdamToken(PtIdamTokenClient.UserType.citizenUser);
+        apiSteps.callIsSubmittedToTheEndpoint("Applications", "GET");
+        apiSteps.checkStatusCode(200);
+        apiSteps.theResponseBodyIsAnEmptyArray();
+    }
+
+    @Title("Applications Endpoint Tests - should return 401 when the S2S token is missing")
+    @Test
+    void applicationsEndpointTest401MissingServiceTokenScenario() {
+        apiSteps.requestIsPreparedWithAppropriateValues();
+        apiSteps.theRequestContainsValidIdamToken(PtIdamTokenClient.UserType.caseworkerUser);
+        apiSteps.callIsSubmittedToTheEndpoint("Applications", "GET");
+        apiSteps.checkStatusCode(401);
+    }
+
+    @Title("Applications Endpoint Tests - should return 401 when the S2S token is invalid")
+    @Test
+    void applicationsEndpointTest401InvalidServiceTokenScenario() {
+        apiSteps.requestIsPreparedWithAppropriateValues();
+        apiSteps.theRequestContainsExpiredServiceToken();
+        apiSteps.theRequestContainsValidIdamToken(PtIdamTokenClient.UserType.caseworkerUser);
+        apiSteps.callIsSubmittedToTheEndpoint("Applications", "GET");
+        apiSteps.checkStatusCode(401);
+    }
+
+    @Title("Applications Endpoint Tests - should return 403 when an unauthorised S2S token is used")
+    @Test
+    void applicationsEndpointTest403Scenario() {
+        apiSteps.requestIsPreparedWithAppropriateValues();
+        apiSteps.theRequestContainsUnauthorisedServiceToken();
+        apiSteps.theRequestContainsValidIdamToken(PtIdamTokenClient.UserType.caseworkerUser);
+        apiSteps.callIsSubmittedToTheEndpoint("Applications", "GET");
+        apiSteps.checkStatusCode(403);
+    }
+}
