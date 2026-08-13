78275f54ea10ece7957e9e9fb5152d860065f2d4
Jibran Walji <jibran.walji@hmcts.net>
2026-08-13 11:01:37 +0100

    HDPD-1108: RaiseAQuery

--- src/main/java/uk/gov/hmcts/reform/pt/ccd/event/EventId.java
+++ src/main/java/uk/gov/hmcts/reform/pt/ccd/event/EventId.java
@@ -35,6 +35,7 @@ public enum EventId {
     ORDERS_CREATE_DRAFT_ORDER("orders-create-draft-order", "Orders: Create draft order"),
     ORDERS_EDIT_DRAFT_ORDER("orders-edit-draft-order", "Orders: Edit draft order"),
     ORDERS_ISSUE_DRAFT_ORDER("orders-issue-draft-order", "Orders: Issue draft order"),
+    RAISE_A_QUERY("raise-a-query", "Raise a query"),
 
     // TEST EVENTS
 
