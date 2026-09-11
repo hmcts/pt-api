39f0c79a89fba0cd8f6c14b1557f07039e090b0d
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-09-11 10:40:56 +0100

    fix: error saving date on current rent details

--- src/main/java/uk/gov/hmcts/reform/pt/util/NullSafeSetter.java
+++ src/main/java/uk/gov/hmcts/reform/pt/util/NullSafeSetter.java
@@ -1,11 +1,19 @@
 package uk.gov.hmcts.reform.pt.util;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.function.Consumer;
 
 public final class NullSafeSetter {
     public static <T> void setIfNotNull(T value, Consumer<T> setter) {
         if (value != null) {
             setter.accept(value);
+        }
+    }
+
+    public static void setIfNotNull(LocalDate value, Consumer<LocalDateTime> setter) {
+        if (value != null) {
+            setter.accept(value.atStartOfDay());
         }
     }
 }
